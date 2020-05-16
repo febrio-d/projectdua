bacf73db6bc891b5de92eeedf5ba1ba713b17625
febrio_d <7g26febrio@gmail.com>
2020-05-16 10:44:24 +0700

    Fragment

--- app/src/main/java/com/smkcoding/projectdua/MainActivity.kt
+++ app/src/main/java/com/smkcoding/projectdua/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.smkcoding.projectdua
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+        val menuTeks = arrayOf("Home", "News", "Profile")
+        val manuIcon = arrayOf(R.drawable.ic_home, R.drawable.ic_news,
+            R.drawable.ic_person)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val adapter = ViewPagerAdapter(this)
+            view_pager.setAdapter(adapter);
+            TabLayoutMediator(tab_layout, view_pager,
+                TabConfigurationStrategy { tab, position ->
+                    tab.text = menuTeks[position]
+                    tab.icon = ResourcesCompat.getDrawable(resources, manuIcon[position], null)
+
+            }).attach()
+        }
     }
-}
---
+++ app/src/main/java/com/smkcoding/projectdua/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.smkcoding.projectdua
+
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+    class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
+
+        private val JUMLAH_MENU = 3
+        override fun createFragment(position: Int): Fragment {
+
+            when (position) {
+                0 -> { return HomeFragment() }
+                1 -> { return NewsFragment() }
+                2 -> { return ProfilFragment() }
+                else -> {
+                    return HomeFragment()
+                }
+            }
+        }
+        override fun getItemCount(): Int {
+            return JUMLAH_MENU
+        }
+    }
